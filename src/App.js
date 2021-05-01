d6fda50f189d60df27cd1c5eb02e6fe94980dc70
Shogo Okuda <shougees@gmail.com>
2021-05-01 12:37:05 +0900

    add more contents

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <br>Shogo Okuda</br>
-          <a href="https://www.linkedin.com/in/shogo-okuda-42b7389/"> LinkedIn </a>
-          
+          Shogo Okuda<br>
+          <a href="https://www.linkedin.com/in/shogo-okuda-42b7389/">LinkedIn</a><br>
+          <a href="https://github.com/shougees">Github</a><br>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
