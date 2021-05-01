193b7cc02144b3eaa5c9da89fa0cecd173e51ce2
Shogo Okuda <shougees@gmail.com>
2021-05-01 13:24:12 +0900

    change list style by map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 //import logo from './logo.svg';import './App.css';
 
 function App() {
+  const bio = {
+    name: 'Shogo Okuda',
+    linkedIn: <a href="https://www.linkedin.com/in/shogo-okuda-42b7389/">LinkedIn</a>,
+    github:   <a href="https://github.com/shougees">Github</a>,
+    twitter: <a href="https://twitter.com/ShogoOkuda">Twitter</a>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Shogo Okuda
-          <a href="https://www.linkedin.com/in/shogo-okuda-42b7389/">LinkedIn</a>
-          <a href="https://github.com/shougees">Github</a>
-        </p>
+        <div>
+          {Object.keys(bio).map(key=> (
+            <p>{bio[key]}</p>
+          ))}
+        </div>
       </header>
     </div>
   );
